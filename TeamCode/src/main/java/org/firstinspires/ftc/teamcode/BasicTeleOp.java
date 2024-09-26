272f989e3d214371b32c00f76319d3af73f16431
gitdapple <perfecttiming1010@gmail.com>
2024-09-26 19:33:23 -0400

    Change the variables and make the manipulator controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -10,8 +10,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-
-
 @TeleOp(name="Basic: Teleop", group="Linear OpMode")
 
 public class BasicTeleOp extends LinearOpMode {
@@ -20,8 +18,8 @@ public class BasicTeleOp extends LinearOpMode {
     private DcMotor rightFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightBackDrive = null;
-    private DcMotor chainMotor1 = null;
-    private DcMotor chainMotor2 = null;
+    private DcMotor ArmMotor = null;
+    private DcMotor ClawAngle = null;
     private DcMotor linearSlide = null;
     private Servo servoClaw = null;
 
@@ -35,28 +33,24 @@ public class BasicTeleOp extends LinearOpMode {
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
         leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
-        chainMotor1 = hardwareMap.get(DcMotor.class, "chainMotor1");
-        chainMotor2 = hardwareMap.get(DcMotor.class, "chainMotor2");
+        ArmMotor = hardwareMap.get(DcMotor.class, "chainMotor1");
+        ClawAngle = hardwareMap.get(DcMotor.class, "chainMotor2");
         linearSlide = hardwareMap.get(DcMotor.class, "linearSlide");
         servoClaw = hardwareMap.get(Servo.class, "servoClaw");
 
 
-        chainMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        chainMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ClawAngle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        double speed = 0.25;
-
-        // chainMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        double speed = 0.5;
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
-
 
 
         // run until the end of the match (driver presses STOP)
@@ -92,11 +86,6 @@ public class BasicTeleOp extends LinearOpMode {
                 rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             }
 
-
-            // chainMotor.setPower(gamepad2.right_stick_y/1.5);
-
-            //chainMotor2.setPower((gamepad2.right_trigger-gamepad2.left_trigger)/2);
-            //untested
             if (gamepad2.left_stick_y > 0.1 || gamepad2.left_stick_y < -0.1) {
                 linearSlide.setPower((gamepad2.left_stick_y));
                 //linearSlide.setPower(0);
@@ -110,55 +99,35 @@ public class BasicTeleOp extends LinearOpMode {
                 speed = 0.25;
             } else if (gamepad1.b) {
                 speed = 0.5;
-            }/* else if (gamepad1.y) {
+            } else if (gamepad1.y) {
                 speed = 0.75;
             } else if (gamepad1.x) {
                 speed = 1;
-            }*/
+            }
 
-            //Intake in and out
+            //Claw open and close
             if (gamepad2.a) {
-                chainMotor1.setPower(0.25);
-            } else if (gamepad2.b) {
-                chainMotor1.setPower(-0.25);
-                chainMotor2.setTargetPosition(-900);
-            } else {
-                chainMotor1.setPower(0);
-            }
-
-            // intake up and down
-            if (gamepad2.y) {
-//500 is about parallel to the ground
-                chainMotor2.setTargetPosition(-800);
-                chainMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                chainMotor2.setPower(0.5);
-            } else if (gamepad2.x) {
-                chainMotor2.setTargetPosition(-1160);
-                chainMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                chainMotor2.setPower(0.5);
-            } else if (gamepad2.right_bumper) {
-                chainMotor2.setTargetPosition(-1070);
-                chainMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                chainMotor2.setPower(0.5);
-            } else if (gamepad2.left_bumper){
-                chainMotor1.setPower(0.5);
-            } else {
-                chainMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                chainMotor2.setPower((gamepad2.right_trigger - gamepad2.left_trigger)/2);
-
-            }
-            if (gamepad2.start) {
                 servoClaw.setPosition(1);
-
-            } else {
+            if (gamepad2.b) {
                 servoClaw.setPosition(0);
             }
+            if (gamepad2.y){
+                ClawAngle.setTargetPosition(100);
+            }
+            if(gamepad2.right_trigger > 0.1){
+                ArmMotor.setTargetPosition(100);
+                sleep(500);
+                ClawAngle.setTargetPosition(-100);
+            }
 
+                // Arm Motor up and down
+                } if (gamepad2.x) {
+                ArmMotor.setTargetPosition(-1160);
+                ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                ArmMotor.setPower(0.5);
 
-
-
+                }
+            }
         }
-
     }
-}
 
