207484c86c76c6ce6f51a095c56d85cce3c83812
Arjun Rao <58148637+raorjun@users.noreply.github.com>
2024-09-30 19:57:24 -0400

    Ri3 w (#4)
    * start dashboard
    
    * added rr
    
    * motor_changes
    
    we changed the name of all the motors and attempted to fix the expansion hub problems via code.
    
    (conclusion: expansion hub is broken?)
    
    ---------
    
    Co-authored-by: gitdapple <perfecttiming1010@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Basic: Teleop", group="Linear OpMode")
+
+public class MotorTest extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightBack = null;
+    private DcMotor ArmMotor = null;
+    private DcMotor ClawAngle = null;
+    private DcMotor linearSlide = null;
+    private Servo servoClaw = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("s", "forlan");
+        telemetry.update();
+
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        ArmMotor = hardwareMap.get(DcMotor.class, "ArmMotor");
+        ClawAngle = hardwareMap.get(DcMotor.class, "ClawAngle");
+        linearSlide = hardwareMap.get(DcMotor.class, "linearSlide");
+        servoClaw = hardwareMap.get(Servo.class, "servoClaw");
+
+
+        ArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        ClawAngle.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        double speed = 0.5;
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+                if (gamepad2.x) {
+                    linearSlide.setPower(0.1);
+                }
+            }
+        }
+    }
+
