266e7e4b8e8c97ca1448d197908addd8c0a0c9d8
gitdapple <perfecttiming1010@gmail.com>
2024-12-03 20:07:50 -0500

    intake clawde ai

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestClaw.java
@@ -7,19 +7,31 @@ import org.firstinspires.ftc.teamcode.subsystems.IntakeClaw;
 
 @TeleOp
 public class TestClaw extends LinearOpMode {
-
     private IntakeClaw claw = new IntakeClaw();
+    private boolean wasAPressed = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
         claw.init(hardwareMap);
 
+        telemetry.addLine("Claw Test OpMode Initialized");
+        telemetry.update();
+
         waitForStart();
 
         while (opModeIsActive()) {
-            if (gamepad1.a){
+            if (gamepad1.a && !wasAPressed) {
                 claw.toggleClaw();
+                telemetry.addLine("Claw Toggled!");
+                telemetry.update();
+                wasAPressed = true;
             }
+
+            if (!gamepad1.a) {
+                wasAPressed = false;
+            }
+
+            sleep(50);
         }
     }
 }
