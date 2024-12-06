21c45689c7ddb257902a518d135a0de697773b2c
gitdapple <perfecttiming1010@gmail.com>
2024-12-06 17:57:00 -0500

    got out test claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Transfer.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import org.firstinspires.ftc.teamcode.subsystems.IntakeClaw;
 
 public class Transfer extends SubsystemParent {
 
